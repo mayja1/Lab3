57aa07a02c324f67b5499139fb6b9070505fbd19
mayja1 <mayja1@rose-hulman.edu>
2015-03-24 15:19:50 -0400

    Added tests for checking prime number generator, fails as there is no such method

---
+++ src/TestGeneratingPrimeNumbers.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+@RunWith(Parameterized.class)
+public class TestGeneratingPrimeNumbers {
+	   private Integer inputNumber;
+	   private int[] expectedResult;
+
+	   // Each parameter should be placed as an argument here
+	   // Every time runner triggers, it will pass the arguments
+	   // from parameters we defined in primeNumbers() method
+	   public TestGeneratingPrimeNumbers(Integer inputNumber, 
+	      List expectedResult) {
+	      this.inputNumber = inputNumber;
+	      this.expectedResult = new int[expectedResult.size()];
+	      for(int i = 0; i < expectedResult.size(); i++) {
+	    	  this.expectedResult[i] = (Integer) expectedResult.get(i);
+	      }
+	   }
+
+	   @Parameterized.Parameters
+	   public static Collection primeNumbers() {
+	      return Arrays.asList(new Object[][] {
+	         { 1, Arrays.asList(new int[] {})},
+	      });
+	   }
+
+	   // This test will run 4 times since we have 5 parameters defined
+	   @Test
+	   public void testPrimeNumberChecker() {
+	      System.out.println("Parameterized Number is : " + inputNumber);
+	      int[] actual =  PrimeFactorization.generatePrimeNumbers(inputNumber);
+	      for(int i = 0; i < expectedResult.length; i++) {
+	      assertEquals(expectedResult[i], actual[i]);
+	      }
+	   }
+}
