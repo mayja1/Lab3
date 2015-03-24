43d45816ccbb739d36c60a52c6d68548c92ea636
mayja1 <mayja1@rose-hulman.edu>
2015-03-24 14:04:13 -0400

    Prime Factorization of 2 passes

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class PrimeFactorization {
 
 	public static int[] generatePrimeFactors(int num) {
-		//todo implement
-		return new int[0];
+		if(num == 1)
+			return new int[0];
+		int[] factors = new int[1];
+		factors[0] = 2;
+		return factors;
 	}
 }
