0ee9d63bea8da080cba4ac100eb38e94128ad28a
mayja1 <mayja1@rose-hulman.edu>
2015-03-24 14:42:54 -0400

    Implemented prime factorization algorithm, all tests pass

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -4,7 +4,27 @@ import java.util.List;
 public class PrimeFactorization {
 
 	public static int[] generatePrimeFactors(int num) {
-		if (num == 1)
+		ArrayList<Integer> factors = new ArrayList<>();
+		while(num%2 == 0) {
+			factors.add(2);
+			num/=2;
+		}
+		int i = 3;
+		while(i <= num) {
+			if (num %i ==0) {
+				factors.add(i);
+				num/=i;
+			} else {
+				i+=2;
+			}
+		}
+		
+		int[] factorArray = new int[factors.size()];
+		for(int j = 0; j < factorArray.length; j++) {
+			factorArray[j] = factors.get(j).intValue();
+		}
+		return factorArray;
+		/*if (num == 1)
 			return new int[0];
 		else if (num == 2 || num == 3) {
 			int[] factors = new int[1];
@@ -24,6 +44,6 @@ public class PrimeFactorization {
 				factors[1] = 3;
 			}
 			return factors;
-		}
+		}*/
 	}
 }
