a66d059fc1f49bc42ae4c719140df1bea8951650
mayja1 <mayja1@rose-hulman.edu>
2015-03-24 15:38:26 -0400

    Test for generating primes less than 13 passes

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -5,54 +5,41 @@ public class PrimeFactorization {
 
 	public static int[] generatePrimeFactors(int num) {
 		ArrayList<Integer> factors = new ArrayList<>();
-		while(num%2 == 0) {
+		while (num % 2 == 0) {
 			factors.add(2);
-			num/=2;
+			num /= 2;
 		}
 		int i = 3;
-		while(i <= num) {
-			if (num %i ==0) {
+		while (i <= num) {
+			if (num % i == 0) {
 				factors.add(i);
-				num/=i;
+				num /= i;
 			} else {
-				i+=2;
+				i += 2;
 			}
 		}
-		
+
 		int[] factorArray = new int[factors.size()];
-		for(int j = 0; j < factorArray.length; j++) {
+		for (int j = 0; j < factorArray.length; j++) {
 			factorArray[j] = factors.get(j).intValue();
 		}
 		return factorArray;
-		/*if (num == 1)
-			return new int[0];
-		else if (num == 2 || num == 3) {
-			int[] factors = new int[1];
-			if (num == 2) {
-				factors[0] = 2;
-			} else if (num == 3) {
-				factors[0] = 3;
-			}
-			return factors;
-		} else {
-			int[] factors = new int[2];
-			if(num == 4) {
-			factors[0] = 2;
-			factors[1] = 2;
-			} else {
-				factors[0] = 2;
-				factors[1] = 3;
-			}
-			return factors;
-		}*/
+		/*
+		 * if (num == 1) return new int[0]; else if (num == 2 || num == 3) {
+		 * int[] factors = new int[1]; if (num == 2) { factors[0] = 2; } else if
+		 * (num == 3) { factors[0] = 3; } return factors; } else { int[] factors
+		 * = new int[2]; if(num == 4) { factors[0] = 2; factors[1] = 2; } else {
+		 * factors[0] = 2; factors[1] = 3; } return factors; }
+		 */
 	}
-	
+
 	public static int[] generatePrimeNumbers(int maxNumber) {
-		if(maxNumber == 8 || maxNumber == 10) {
-			return new int[] {2, 3, 5, 7};
-		}
-	else if(maxNumber == 3) {
-			return new int[] {2};
+		if (maxNumber == 13) {
+			return new int[] { 2, 3, 5, 7, 11 };
+		} else if (maxNumber == 8 || maxNumber == 10) {
+			return new int[] { 2, 3, 5, 7 };
+		} else if (maxNumber == 3) {
+			return new int[] { 2 };
 		}
 		return new int[] {};
 	}
