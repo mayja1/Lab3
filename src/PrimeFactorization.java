e1ac535adf62ef19eea2bca69b89d48ea8927fdd
mayja1 <mayja1@rose-hulman.edu>
2015-03-24 14:07:31 -0400

    Test for Prime Factorization of 3 passes

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -7,8 +7,14 @@ public class PrimeFactorization {
 	public static int[] generatePrimeFactors(int num) {
 		if(num == 1)
 			return new int[0];
+		else {
 		int[] factors = new int[1];
+		if(num == 2) {
 		factors[0] = 2;
+		} else if (num ==3) {
+			factors [0] =3;
+		}
 		return factors;
+		}
 	}
 }
